e311b284d51dadd6dfe52752e8c616c51d7859f7
Lluis Espert Cuquerella <lluisespertc@gmail.com>
2023-06-12 12:38:13 +0200

    Botones creados
    He creado los botones, que de momento me son necesarios

---
+++ vista/inicio.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Radio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+    <script type="text/javascrtipt" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container mt-3">
+  <button type="button" class="btn btn-outline-success">Programas</button>  
+  <button type="button" class="btn btn-outline-success">Eventos</button>
+  <button type="button" class="btn btn-outline-success">Podcasts</button>
+  <button type="button" class="btn btn-outline-success">Sellos</button>
+</div>
+</body>
+</html>
